8f8de7531c5858003b11497b3cc6bcd3d8c74fc0
Omar Abdelhafith <o.arrabi@me.com>
2016-01-24 23:50:56 +0000

    Making EventMonitor internal

--- Pod/Classes/EventMonitor.swift
+++ Pod/Classes/EventMonitor.swift
@@ -22,12 +22,12 @@
 
 import Cocoa
 
-public class EventMonitor {
+class EventMonitor {
   private var monitor: AnyObject?
   private let mask: NSEventMask
   private let handler: NSEvent? -> ()
 
-  public init(mask: NSEventMask, handler: NSEvent? -> ()) {
+  init(mask: NSEventMask, handler: NSEvent? -> ()) {
     self.mask = mask
     self.handler = handler
   }
@@ -36,11 +36,11 @@ public class EventMonitor {
     stop()
   }
 
-  public func start() {
+  func start() {
     monitor = NSEvent.addGlobalMonitorForEventsMatchingMask(mask, handler: handler)
   }
 
-  public func stop() {
+  func stop() {
     if monitor != nil {
       NSEvent.removeMonitor(monitor!)
       monitor = nil
